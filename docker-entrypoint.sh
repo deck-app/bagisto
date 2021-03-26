f88d2f801bb011d477563009177dbc8263edc978
nabad600 <nabad600@gmail.com>
2021-03-26 18:18:02 +0530

    env file sed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,17 +6,21 @@ echo "Git already exists"
 else
 git clone https://github.com/nabad600/bagisto.git .
 echo "Download completed..."
-chown -R apache:apache /var/www
-fi
-cp .env.testing .env
+#chown -R apache:apache /var/www
+#cp .env.testing .env
+cp /sbin/app /var/www/.env
+#echo $DB_USER
 echo ".env file copy done."
-sed -i 's#{DB_NAME}#$DB_NAME#g' /var/www/.env
-sed -i 's#{DB_USER}#$DB_USER#g' /var/www/.env
-sed -i 's#{DB_PASSWORD}#$DB_PASSWORD#g' /var/www/.env
+#sed -i "s/$DB_USER/bagisto/g" /var/www/.env
+#sed -i 's#{DB_NAME}#$DB_NAME#g' /var/www/.env
+#sed -i 's#{DB_USER}#$DB_USER#g' /var/www/.env
+#sed -i 's#{DB_PASSWORD}#$DB_PASSWORD#g' /var/www/.env
 echo ".env file value change."
+fi
 composer install
 echo "File permission."
 chown -R apache:apache /var/www
+chmod -R 777 storage
 #/public/installer
 #php artisan migrate
 
